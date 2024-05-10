540dac3c1538b81dd5b4a55896b556f0edf03ae3
nerzh <emptystamp@gmail.com>
2024-05-10 02:11:44 +0200

    refactor

--- Sources/FileUtils/FileReader.swift
+++ Sources/FileUtils/FileReader.swift
@@ -44,7 +44,23 @@ class FileReader {
         guard let descriptor = self.file else {
             throw NSError(domain: NSPOSIXErrorDomain, code: Int(EBADF), userInfo: nil)
         }
-        var buffer = [CChar](repeating: 0, count: maxLength)
+        var buffer = Array<Int8>.init(repeating: 0, count: maxLength)
+        guard fgets(&buffer, Int32(maxLength), descriptor) != nil else {
+            if feof(descriptor) != 0 {
+                return nil
+            } else {
+                throw NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil)
+            }
+        }
+        
+        return String(cString: buffer)
+    }
+    
+    func readLine(maxLength: Int = 4096) throws -> Data? {
+        guard let descriptor = self.file else {
+            throw NSError(domain: NSPOSIXErrorDomain, code: Int(EBADF), userInfo: nil)
+        }
+        var buffer = Array<Int8>.init(repeating: 0, count: maxLength)
         guard fgets(&buffer, Int32(maxLength), descriptor) != nil else {
             if feof(descriptor) != 0 {
                 return nil
@@ -53,6 +69,27 @@ class FileReader {
             }
         }
 
-        return String(cString: buffer)
+        let newBuffer: Array<UInt8> = unsafeBitCast(buffer, to: Array<UInt8>.self)
+
+        return Data(newBuffer)
     }
+    
+//    func readLine(maxLength: Int = 4096) throws -> Data? {
+//        if let stream: InputStream = InputStream(fileAtPath: fileURL.path) {
+//            var buf: [UInt8] = [UInt8](repeating: 0, count: 16)
+//            stream.open()
+//            while true {
+//                let len = stream.read(&buf, maxLength: buf.count)
+//                print("len \(len)")
+////                c += len
+////                for i in 0..<len {
+////                    print(String(format:"%02x ", buf[i]), terminator: "")
+////                }
+//                if len < buf.count {
+//                    break
+//                }
+//            }
+//            stream.close()
+//        }
+//    }
 }
--- Sources/FileUtils/ReadFile.swift
+++ Sources/FileUtils/ReadFile.swift
@@ -12,30 +12,47 @@ public extension FileUtils {
     class func readFile(_ url: URL, encoding: String.Encoding = .utf8) throws -> String {
         try String(contentsOf: url, encoding: encoding)
     }
+    
+    class func readFile(_ url: URL) throws -> Data {
+        try Data(contentsOf: url)
+    }
 
-    class func readFileByLine(_ path: String, _ handler: (_ line: String) -> Void) throws {
+    class func readStringByLine(_ path: String, _ handler: (_ line: String) throws -> Void) throws {
         let file: FileReader = .init(filePath: path)
         try file.open()
         defer { file.close() }
         while let line: String = try file.readLine() {
-            handler(line)
+            try handler(line)
         }
     }
 
-    class func readFileByLine(_ url: URL, _ handler: (_ line: String) -> Void) throws {
-        try readFileByLine(url.path, handler)
+    class func readStringByLine(_ url: URL, _ handler: (_ line: String) throws -> Void) throws {
+        try readStringByLine(url.path, handler)
+    }
+    
+    class func readFileByLine(_ url: URL, maxLength: Int = 4096, _ handler: (_ line: Data) throws -> Void) throws {
+        try readFileByLine(url.path, maxLength: maxLength, handler)
+    }
+    
+    class func readFileByLine(_ path: String, maxLength: Int = 4096, _ handler: (_ line: Data) throws -> Void) throws {
+        let file: FileReader = .init(filePath: path)
+        try file.open()
+        defer { file.close() }
+        while let line: Data = try file.readLine(maxLength: maxLength) {
+            try handler(line)
+        }
     }
 
-    class func readFileByLine(_ path: String, _ handler: (_ line: String) -> Bool) throws {
+    class func readStringByLineWithBool(_ path: String, _ handler: (_ line: String) throws -> Bool) throws {
         let file: FileReader = .init(filePath: path)
         try file.open()
         defer { file.close() }
         while let line: String = try file.readLine() {
-            if !handler(line) { break }
+            if try !handler(line) { break }
         }
     }
 
-    class func readFileByLine(_ url: URL, _ handler: (_ line: String) -> Bool) throws {
-        try readFileByLine(url.path, handler)
+    class func readStringByLineWithBool(_ url: URL, _ handler: (_ line: String) throws -> Bool) throws {
+        try readStringByLineWithBool(url.path, handler)
     }
 }
